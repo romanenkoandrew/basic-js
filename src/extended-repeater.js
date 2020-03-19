4a058d69033ca07cd490c6456e74def95cd1f0a5
RomanenkoAndrew <romanenkoandrey95@mail.ru>
2020-03-19 20:15:34 +0300

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+   
+     let repeatTimes = options.repeatTimes;
+     let separator = options.separator || '+';
+     let addition = options.addition;
+     let additionRepeatTimes = options.additionRepeatTimes;
+     let additionSeparator = options.additionSeparator || '|';
+     let newStr = ''
+     // Проверка addition
+     typeof(addition) == 'boolean' ? addition = addition.toString() :
+     addition === null ? addition ='null' : 
+     addition
+     // Получение добавочной строки
+     newStr = (addition + additionSeparator).repeat(additionRepeatTimes-1) + addition;
+     // Получение конечной строки
+     addition ? str = (str + newStr + separator).repeat(repeatTimes-1) + str+newStr : str = (str + separator).repeat(repeatTimes-1) + str
+      
+     return str
 };
   
